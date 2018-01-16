d33f3ec7a0a28ab9f52f857bf1a02a089efb22dc
Jason Welsh <--global>
2018-01-16 17:27:08 -0500

    fixed search bug and added svg button icon as component rendered inside of button

--- src/Nav/SearchForm.js
+++ src/Nav/SearchForm.js
@@ -2,6 +2,49 @@ import React, { Component } from 'react';
 import Container from '../Container';
 import Navigation from './Navigation';
 
+// create component for MAg Glass SVG
+// rendered inside <button> element
+const MagGlass = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 451 451" width="25" height="25">
+    <g>
+    	<path d="M447.05,428l-109.6-109.6c29.4-33.8,47.2-77.9,47.2-126.1C384.65,86.2,298.35,0,192.35,0C86.25,0,0.05,86.3,0.05,192.3   s86.3,192.3,192.3,192.3c48.2,0,92.3-17.8,126.1-47.2L428.05,447c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4   C452.25,441.8,452.25,433.2,447.05,428z M26.95,192.3c0-91.2,74.2-165.3,165.3-165.3c91.2,0,165.3,74.2,165.3,165.3   s-74.1,165.4-165.3,165.4C101.15,357.7,26.95,283.5,26.95,192.3z" fill="#FFFFFF"/>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    <g>
+    </g>
+    </svg>
+  );
+}
+
+
 class SearchForm extends Component  {
 
   state = {
@@ -14,7 +57,7 @@ class SearchForm extends Component  {
 
   handleSubmit = e => {
     e.preventDefault();
-    this.props.onSearch(this.query.value);
+    this.setState({ query:this.state.query });
     e.currentTarget.reset();
   }
 //renders the Search Component, Nav Component, and the Container component.
@@ -22,13 +65,13 @@ class SearchForm extends Component  {
     return(
       <div>
         <form className="search-form" onSubmit={this.handleSubmit} >
-          <label htmlFor="search">Search</label>
+          <label class="form-label" htmlFor="search">Search</label>
           <input type="search"
                   onChange={this.onSearchChange}
                   name="search"
                   ref={(input) => this.query = input}
                   placeholder="Search..." />
-          <button type="submit" id="submit" className="search-button">svg icon here...</button>
+          <button type="submit" id="submit" className="search-button"><MagGlass /></button>
         </form>
         <Navigation />
         <h2>{this.state.query}</h2>
